f7eab976cd1598d80328ca0ac4ae53464dad7264
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-09 18:31:00 -0400

    Add further description

--- challenge1.js
+++ challenge1.js
@@ -4,7 +4,9 @@
 // The goal for this problem is to identify all the string elements inside the array with only 4 characters.
 
 // I decided to use a for loop to evaluate each element inside the array. The evaluation starts at index 0 and recalls each 
-// 4 letter character through the .length method. I had set the method to 4 characters which would then print out each string
+// 4 letter character through the .length method. I did this by creating the i variable creating assigning it an idex of 0.
+// I then made i be less than total elements in the array. I then had the for loop evaluate each string one at a time with i++. 
+// Within the if statement, I had set the method to 4 characters which would then print out each string
 // that was flagged with 4 characters.
 
 var words = ["dog", "read", "Papagorgio", "tire"]
--- challenge2.js
+++ challenge2.js
@@ -3,8 +3,10 @@
 
 // The goal is to make all strings in the array lowercase. 
 // I knew I needed to use the .toLowerCase method, but was unsure how to apply to the entire array. After searching online
-// I discovered the .map method which creates a new array. I knew I could create an accompanying function. So I used recalled a function
-// inside .map's argument and held a place value "s" in the function's argument. I then returned the array with s.toLowerCase().
+// I discovered the .map method which creates a new array. I knew I could create an accompanying function. This is what I understand
+// to be a nested function. Or a function within a function. In this case I used it within the .map method. 
+// So I used recalled a function inside .map's argument and held a place value "s" in the function's argument. 
+// I then returned the array with s.toLowerCase().
 // From there, I could recall the words variable in the console.log with the new array all in lowercase lettering. 
 
 var words = ["Bolivia", "Bhutan", "East Timor", "Dardanelles", "Galicia"].map(function(s) {
